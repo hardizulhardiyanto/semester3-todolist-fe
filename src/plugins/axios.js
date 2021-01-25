70a4d596ac2d2c830baf515e40e8e84b91945eab
Zulhardiyanto Hardi <zulhardiyanto.achmad@bts.id>
2021-01-25 16:27:41 +0700

    plugins axios created

---
+++ src/plugins/axios.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import axios from 'axios';
+
+let config = {
+    baseURL: "http://localhost:3000"
+}
+
+const _axios = axios.create(config)
+
+Plugin.install = function (Vue) {
+    Vue.axios = _axios
+    window.axios = _axios
+    Object.defineProperty(Vue.prototype, {
+        axios: {
+            get() {
+                return _axios;
+            }
+        },
+        $axios: {
+            get() {
+                return _axios
+            }
+        }
+    })
+}
+
+Vue.use(Plugin);
+export default Plugin
